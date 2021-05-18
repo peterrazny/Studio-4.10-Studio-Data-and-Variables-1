4afb248ff24486fcb49bbb445aa1ef314789d1ac
PeterRazny <>
2021-05-18 00:57:11 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// FORK this starter file and save it to your own Repl.it account.
+
+
+// Declare and initialize the 12 variables here:
+let date = "Monday 2019-03-18";
+let time = "10:05:31 AM";
+let astronaut.Count
+console.log(time);
+
+
+
+// Write code to generate the LC04 report here:
+
+
+
+
+// When done, have your TA check your code.
+
+
+
+
+// BONUS: Use readline-sync to prompt the user to enter the number of astronauts going on the mission.
